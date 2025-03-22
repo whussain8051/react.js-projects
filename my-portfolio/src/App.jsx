b5fe5f85c8c29b265e84b566e20f509ba97fc438
Waqar Hussain <whussain8051@gmail.com>
2025-03-22 22:42:53 +0500

    Portfolio Start

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,7 +1,12 @@
+import Navbar from './components/Navbar';
+
 const App = () => {
   return (
     <>
-    
+    <div className='container-fluid'>
+
+      <Navbar />
+    </div>
     </>
   )
 }
--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './App.jsx'
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
