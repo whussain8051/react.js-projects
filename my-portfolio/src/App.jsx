14e4c1d6fddbf5e1c3a991258be1eac7efcdaf3b
Waqar Hussain <whussain8051@gmail.com>
2025-03-23 23:39:34 +0500

    Portfolio update

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -4,7 +4,6 @@ const App = () => {
   return (
     <>
     <div className='container-fluid'>
-
       <Navbar />
     </div>
     </>
--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -3,22 +3,14 @@ const Navbar = () => {
         <>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <a class="navbar-brand" href="#">Navbar</a>
-                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
-                        </div>
-                    </div>
-
-                    <div className="col-md-6">
-                        
-                   
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                <a class="nav-link" aria-current="page" href="#">Home</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="#">Features</a>
@@ -27,10 +19,9 @@ const Navbar = () => {
                                 <a class="nav-link" href="#">Pricing</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                                <a class="nav-link" aria-disabled="true">Disabled</a>
                             </li>
                         </ul>
-                    </div>
                     </div>
                 </div>
             </nav>
